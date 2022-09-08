2cb7323f463e47d486b066a0b5b882a1c47c8488
Sharon97john <110244759+Sharon97john@users.noreply.github.com>
2022-09-08 16:30:15 +0100

    Delete backend directory

--- backend/department/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import department, department_head, department_types
-
-# Register your models here.
-admin.site.register(department)
-admin.site.register(department_head)
-admin.site.register(department_types)
